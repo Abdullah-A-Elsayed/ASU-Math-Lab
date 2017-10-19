f2d181fdba50567cf5d4812054e3dae1ac451f5c
Abdullah-A-Elsayed <abdullah.ali26@hotmail.com>
2017-10-19 20:23:04 +0200

    initializing matrix and printing

--- index.cpp
+++ index.cpp
@@ -11,6 +11,14 @@ public:
     matrix(int rows, int cols){// constructor
         this->num_rows = rows;
         this->num_columns = cols;
+         //pushing values with zeros (initialization)
+         for(int i=0 ; i<rows ; ++i){
+            vector<int> row;
+            for(int j=0 ; j<cols ; ++j){
+                row.push_back(0);
+            }
+            values.push_back(row);
+        }
     }
     int get_num_rows (){
         return this->num_rows;
@@ -18,10 +26,18 @@ public:
     int get_num_columns (){
         return this->num_columns;
     }
+    void print_matrix(){ // for testing
+        for(int i=0 ; i< this-> num_rows ; ++i){
+            for(int j=0 ; j< this->num_columns ; ++j){
+                cout<<values[i][j]<<"    ";
+            }
+            cout<<endl;
+        }
+    }
 
 };
 int main(){
     matrix u(3,5);
-    cout<<u.get_num_columns()<<endl; //check
+    u.print_matrix();
     return 0;
 }
