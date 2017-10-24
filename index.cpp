ed4c4a78f09c1842542f6745aa8447226ffef6cd
gassersamy <gassersamy7@gmail.com>
2017-10-24 17:04:10 +0200

    Inverse func done

--- index.cpp
+++ index.cpp
@@ -36,7 +36,7 @@ public:
         return this->num_columns;
     }
 
-    void print_matrix(){ // for testing
+    void print_matrix(){ // print matrix for testing
         for(int i=0 ; i< this-> num_rows ; ++i){
             for(int j=0 ; j< this->num_columns ; ++j){
                 cout<<this->values[i][j]<<"    ";
@@ -91,13 +91,145 @@ public:
         // return result
     }
 
-    matrix inverse_matrix(){
-        //gasser
-        //create a result matrix with correct dimensions then initialize it using initialize function provided above
-        // result = this inverse
-        // return result
-        // good luck
+	/*------------------------------------------------START GASSER inverse-mat--------------------------------------------------------------*/
+	
+	// fill matrix with test values
+	void fill_mat_test()
+	{
+		this->values[0][0] = 3; this->values[0][1] = 0; this->values[0][2] = 2;
+		this->values[1][0] = 2; this->values[1][1] = 0; this->values[1][2] = -2;
+		this->values[2][0] = 0; this->values[2][1] = 1; this->values[2][2] = 1;
+	}
+
+	// calculate determine func
+	float cal_determin_sq(int num_rows)
+	{
+		float s = 1, det = 0; 
+		matrix b; b.initialize(this->num_rows, this->num_columns);
+		int i, j, m, n, c, k = num_rows;
+		if (k == 1)
+		{
+			return (this->values[0][0]);
+		}
+		else
+		{
+			det = 0;
+			for (c = 0; c<k; c++)
+			{
+				m = 0;
+				n = 0;
+				for (i = 0; i<k; i++)
+				{
+					for (j = 0; j<k; j++)
+					{
+						b.values[i][j] = 0;
+						if (i != 0 && j != c)
+						{
+							b.values[m][n] = this->values[i][j];
+							if (n<(k - 2))
+								n++;
+							else
+							{
+								n = 0;
+								m++;
+							}
+						}
+					}
+				}
+				det = det + s * (this->values[0][c] * b.cal_determin_sq((k - 1)));
+				s = -1 * s;
+			}
+		}
+
+		return (det);
+	}
+
+	// calculate co-factor func
+	matrix cal_cofactor(int num_rows)
+	{
+		matrix b; b.initialize(this->num_rows, this->num_columns);
+		matrix fac; fac.initialize(this->num_rows, this->num_columns);
+		matrix inverse; inverse.initialize(this->num_rows, this->num_columns);
+		int f = num_rows;
+		int p, q, m, n, i, j;
+		for (q = 0; q < f; q++)
+		{
+			for (p = 0; p < f; p++)
+			{
+				m = 0;
+				n = 0;
+				for (i = 0; i < f; i++)
+				{
+					for (j = 0; j < f; j++)
+					{
+						if (i != q && j != p)
+						{
+							b.values[m][n] = this->values[i][j];
+							if (n < (f - 2))
+								n++;
+							else
+							{
+								n = 0;
+								m++;
+							}
+						}
+					}
+				}
+				fac.values[q][p] = pow(-1, q + p) * b.cal_determin_sq((f - 1));
+			}
+		}
+		
+		return (this->transpose(fac, f));
+	}
+
+	// find transpose func
+	matrix transpose(matrix fac, float r)
+	{
+		int i, j;
+		matrix b; b.initialize(this->num_rows, this->num_columns);
+		matrix inverse; inverse.initialize(this->num_rows, this->num_columns);
+		float d;
+
+		for (i = 0; i<r; i++)
+		{
+			for (j = 0; j<r; j++)
+			{
+				b.values[i][j] = fac.values[j][i];
+			}
+		}
+		d = this->cal_determin_sq(r);
+		for (i = 0; i<r; i++)
+		{
+			for (j = 0; j<r; j++)
+			{
+				inverse.values[i][j] = b.values[i][j] / d;
+			}
+		}
+		return inverse;
+	}
+
+    // find inverse matrix
+	matrix inverse_matrix()
+	{
+		float det_val = this->cal_determin_sq(this->num_rows); // calculate determine value for the matrix
+
+		// intialize the inverse matrix with zeros & check if it is a square matrix
+		matrix m; m.initialize(this->num_rows, this->num_columns); 
+		if (this->num_rows != this->num_columns){ cout << "No inverse for non-square matrix" << endl; return (m); }
+
+		// check if determine equals zero
+		else if (det_val == 0){ cout << "No inverse for this zero-determine matrix" << endl; return (m); }
+
+		// strat to get the inverse for the matrix
+		else
+		{
+			m = this->cal_cofactor(this->num_rows);
+			return (m);
+		}
+
     }
+
+	/*------------------------------------------------END GASSER inverse-mat--------------------------------------------------------------*/
 
     matrix div_matrix( matrix m){
         //Alaa Ayman
@@ -106,7 +238,22 @@ public:
         // use previous functions
     }
 };
-int main(){
-    
+
+int main()
+{
+	/* 
+		// code test for inverse
+
+		matrix x; matrix y;
+		x.initialize(3, 3);
+		x.fill_mat_test();
+		x.print_matrix();
+		cout << endl;
+		y= x.inverse_matrix();
+		cout << "Inverse Matrix\n" << endl;
+		y.print_matrix();
+		cout << endl;
+	*/
+
     return 0;
 }
