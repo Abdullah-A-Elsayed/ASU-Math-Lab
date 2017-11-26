caa58c80c71b9075c8301fdd2166f355a233dd08
unknown <ayasayed_711@yahoo.com>
2017-11-26 21:37:31 +0200

    aya

--- matrix.h
+++ matrix.h
@@ -41,6 +41,102 @@ public:
     static void handle_read(map<const string, matrix>& matrices,string command,string name0,int op_index);
     static void decode(string command,string& name1,string& name2,int op_index);
     static void remove_back_slashes(string& s);
+	
+
+
+	
+	void Solve(string data);//AMERA
+	/*take data as A= 5.5 + 12 * sin(0.4) + 2.2^4
+   	and return value */
+	
+	void fill_matrix2(string data);//AYA
+	/* take data and fill matrix
+	B = [1.2 2.3 A;[1.3 2.4;4.6 1.3],[3.2;7.8]];
+	B =
+
+    1.2000    2.3000   33.5986
+    1.3000    2.4000    3.2000
+    4.6000    1.3000    7.8000
+
+                   */
+
+	void fill_matrix3(string data);  //ALAA
+	/* taka data and fill matrix
+	C = [[B [3.4; 2.1; 3.5+9.1]]
+     1.2^3 3+1.2 15/(2.1+10*sin(0.12))  1.2]
+	 C =
+    1.2000    2.3000   33.5986    3.4000
+    1.3000    2.4000    3.2000    2.1000
+    4.6000    1.3000    7.8000   12.6000
+    1.7280    4.2000    4.5494    1.2000
+
+	
+	*/
+
+	matrix Sin();//AYA
+	/* sin(B)
+ans =
+
+   0.932039   0.745705   0.818560
+   0.963558   0.675463  -0.058374
+  -0.993691   0.963558   0.998543
+*/
+	matrix Sqrt();//AYA
+	/* sqrt(B)
+ans =
+
+   1.0954   1.5166   5.7964
+   1.1402   1.5492   1.7889
+   2.1448   1.1402   2.7928*/
+	matrix Pow(int n);//AYA
+	/*  B^2
+ans =
+
+   158.984    51.958   309.748
+    19.400    12.910    76.318
+    43.090    23.840   219.554
+*/
+
+	matrix Rand(int a,int b);//AMERA
+	/*rand(4,4)
+	ans =
+
+   0.506569   0.918578   0.739724   0.105990
+   0.426771   0.614705   0.318234   0.448953
+   0.510929   0.436886   0.610394   0.190750
+   0.713867   0.698260   0.013628   0.215899
+
+
+	*/
+	matrix Eye (int a,int b);//AMERA
+	/*eye(4, 4)
+
+Diagonal Matrix
+
+   1   0   0   0
+   0   1   0   0
+   0   0   1   0
+   0   0   0   1  */
+	matrix Zeros (int a,int b);//ALAA
+	/*zeros(2, 3)
+	ans =
+   0   0   0
+   0   0   0
+	*/
+
+	matrix ones (int a,int b);//ALAA
+	/*ones(3, 6)
+	ans =
+   1   1   1   1   1   1
+   1   1   1   1   1   1
+   1   1   1   1   1   1
+	*/
+	
+	
+	
+
+
+    
 };
 
 #endif
