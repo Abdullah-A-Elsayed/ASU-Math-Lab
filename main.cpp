0586d9b2c452f553a5984be21401aae02ee9cd12
Abdullah-A-Elsayed <abdullah.ali26@hotmail.com>
2018-01-20 13:01:57 +0200

    optimizing main by optimizing phase1

--- main.cpp
+++ main.cpp
@@ -10,19 +10,18 @@ int main(int argc, char** argv)
 { 	
 if(argc < 2){//no file passed to the program
 		/***********************************taking commands***************************************/
-		cout<<"Enter your commands ..."<<endl; 
-		string command;
-		map<const string, matrix> matrices;
-		while(1){
-			getline(cin,command);
-			try{
-				matrix::run_old_command(command,matrices);
-			}
-			catch(string e){ cout<<e<<endl;}
-		}
+		//cout<<"Enter your commands ..."<<endl; 
+		//string command;
+		//map<const string, matrix> matrices;
+		//while(1){
+		//	getline(cin,command);
+		//	try{
+		//		matrix::run_old_command(command,matrices);
+		//	}
+		//	catch(string e){ cout<<e<<endl;}
+		//}
 		/*************************************testing area***************************************/
 	
-
 }
 else{ // passing file as a command argument
 	string fpath = argv[1]; //file name
